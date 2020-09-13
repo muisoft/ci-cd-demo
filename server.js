c1930359fc942e5f40e71ee9766726045dc419ce
Muideen Adewale Abdulrasheed <muisoft2016@gmail.com>
2020-09-13 19:07:24 +0100

    added express

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
-const app = require('http');
+const express = require('express');//require('http');
 
-app.createServer((req, res) => {
-    console.log("Hello")
+// app.createServer((req, res) => {
+//     console.log("Hello")
+// })
+const app = express();
+app.get((req, res) => {
+   console.log("Hello")
 })
 
 app.listen(30000);
